a01417d72325b3ad3023ceb1a90940e02ea5af36
Craig Rodger <crrodger@microsoft.com>
2020-03-13 11:40:59 +1100

    Trigger both repo build

--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -7,7 +7,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     if (name) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Trigger both repos " + (req.query.name || req.body.name)
+            body: "Trigger both repo builds Fri 11:40 " + (req.query.name || req.body.name)
         };
     }
     else {
